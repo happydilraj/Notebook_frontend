aff753b683dc5767465974f879fcd1e17d65e548
happydilraj <happydilraj1022@gmail.com>
2023-06-16 01:07:07 +0530

    copied icon

--- src/Component/NotesItem.jsx
+++ src/Component/NotesItem.jsx
@@ -20,6 +20,14 @@ const NotesItem = (props) => {
     setisAlert(5)
   }
 
+  const copyFunction = (description)=>{
+    navigator.clipboard.writeText(description);
+    setmessage("Description copied!")
+    settype("primary")
+    fun()
+    setisAlert(5)
+  }
+
   return (
 
     <>
@@ -27,7 +35,7 @@ const NotesItem = (props) => {
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title">{note.title} <i onClick={()=>{ deleteNoteFunction(note._id)}} className="fa-solid fa-trash mx-2"></i> <i className="fa-solid mx-2 fa-pen-to-square" onClick={()=>{updateNote(note)}}></i></h5>
+          <h5 className="card-title">{note.title} <i onClick={()=>{ deleteNoteFunction(note._id)}} className="fa-solid fa-trash mx-2"></i> <i className="fa-solid mx-2 fa-pen-to-square" onClick={()=>{updateNote(note)}}></i><i onClick={()=>{copyFunction(note.description)}} className="fa-solid fa-copy mx-2"></i></h5>
           <p className="card-text">
             {note.description}
           </p>
